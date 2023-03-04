0ac9fe8b7054206bbf648d4b72de7aca1d5ce160
cslavica <curicsla@gmail.com>
2023-03-04 20:30:55 +0100

    Ucenici

---
+++ public/js/students.js
@@ -0,0 +1,6 @@
+const studentsA = document.getElementById("students");
+const main = document.getElementById("main");
+
+studentsA.addEventListener('click', () => {
+        main.classList.add("li-clicked3");
+});
